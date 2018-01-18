196e737108e5000751eae6d94d98ba81649db00a
Sarah F <sarahelayna@gmail.com>
2018-01-18 03:45:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ return obj
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({},obj);
+  var newObj = Object.assign({},object);
   delete newObj[key];
   return newObj;
 }
