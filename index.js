0137fec3c19979ce0d1b98cb50352546a5e3b6eb
Sarah F <sarahelayna@gmail.com>
2018-01-18 03:40:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 var obj = {[key]: value}
 return obj
 }
+
+function deleteFromObjectByKey(object, key) {
+  var obj = {[key]: value};
+  var newObj = delete obj.key;
+}
